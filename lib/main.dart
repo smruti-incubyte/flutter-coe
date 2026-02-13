b4ea967a130eb6dfb9203b8e31d14851d2d3c6a6
smruti-incubyte <smruti@incubyte.co>
2026-02-13 15:29:39 +0530

    (DAY 9) Integrate Firebase Core into the project, updating dependencies in pubspec.yaml and pubspec.lock. Configure Firebase initialization in main.dart and update Android and iOS build files for Firebase support. Enhance .gitignore for Fastlane environment variables.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart' show Firebase;
 import 'package:flutter/material.dart';
+import 'package:flutter_coe/firebase_options.dart' show DefaultFirebaseOptions;
 import 'screens/home_screen.dart';
 import 'screens/responsive_navigation_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
